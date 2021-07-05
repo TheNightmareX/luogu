b9df2ad7b9f30faf06fa1883913929672d371244
TheNightmareX <TheNightmareX@foxmail.com>
2021-07-05 21:18:31 +0800

    fix(solutions): fix p2280

--- solutions/p2280/index.cpp
+++ solutions/p2280/index.cpp
@@ -9,16 +9,20 @@ int main() {
 
     int matrix[MAX_MATRIX_BORDER_LENGTH + 5][MAX_MATRIX_BORDER_LENGTH + 5];
 
+    int max_row_num = 0, max_col_num = 0;
     for (int i = 1; i <= item_count; i++) {
         int row_num, col_num, value;
         cin >> row_num >> col_num >> value;
         row_num++, col_num++;
         matrix[row_num][col_num] = value;
+
+        max_row_num = max(max_row_num, row_num);
+        max_col_num = max(max_col_num, col_num);
     }
 
-    int answer = 0;
-    for (int row_num = 1; row_num <= MAX_MATRIX_BORDER_LENGTH; row_num++)
-        for (int col_num = 1; col_num <= MAX_MATRIX_BORDER_LENGTH; col_num++) {
+    int answer = 0; 
+    for (int row_num = 1; row_num <= max(max_row_num, range_border_length); row_num++)
+        for (int col_num = 1; col_num <= max(max_col_num, range_border_length); col_num++) {
             matrix[row_num][col_num] += matrix[row_num - 1][col_num];
             matrix[row_num][col_num] += matrix[row_num][col_num - 1];
             matrix[row_num][col_num] -= matrix[row_num - 1][col_num - 1];
