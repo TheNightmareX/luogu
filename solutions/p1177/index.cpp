f223120973b08955fd31276ba1e1af053a969fe2
TheNightmareX <TheNightmareX@foxmail.com>
2021-07-07 23:39:43 +0800

    feat(solutions): update p1177

--- solutions/p1177/index.cpp
+++ solutions/p1177/index.cpp
@@ -5,18 +5,20 @@ void quick_sort(int *numbers, int start, int end) {
     if (end - start <= 1)
         return;
 
-    int base_num = numbers[start];
+    // avoid always picking the most front number as the pivot number so that the worst situation will not appear every time
+    int length = end - start;
+    swap(numbers[start], numbers[rand() % length + start]);
 
     int index_to_swap = start + 1;
     for (int i = index_to_swap; i < end; i++)
-        if (numbers[i] < base_num)
+        if (numbers[i] < numbers[start])
             swap(numbers[index_to_swap++], numbers[i]);
 
-    // numbers left side are all smaller or equal and right side are all greater or equal.
-    int center_index = index_to_swap - 1;
-    swap(numbers[start], numbers[center_index]);
-    quick_sort(numbers, start, center_index);
-    quick_sort(numbers, center_index + 1, end);
+    // numbers left side are all smaller or equal and right side ones are all greater or equal than the number with the pivot index.
+    int pivot_index = index_to_swap - 1;
+    swap(numbers[start], numbers[pivot_index]);
+    quick_sort(numbers, start, pivot_index);
+    quick_sort(numbers, pivot_index + 1, end);
 }
 
 int main() {
