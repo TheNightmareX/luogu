063c71fdafb04e8218d0ae69d44b9e5040dd6516
TheNightmareX <TheNightmareX@foxmail.com>
2021-09-20 22:05:42 +0800

    feat(solutions): update p1216

--- solutions/p1216/p1216.cpp
+++ solutions/p1216/p1216.cpp
@@ -2,24 +2,30 @@
 using namespace std;
 
 int row_count;
-vector< vector<int> > matrix;
+vector< vector< pair<int, int> > > matrix;
 
 int dp(int row, int col) {
-    int value = matrix[row][col];
     if (row == row_count - 1)
-        return value;
-    return max(dp(row + 1, col), dp(row + 1, col + 1)) + value;
+        return matrix[row][col].first;
+
+    int ret = matrix[row][col].second != -1
+      ? matrix[row][col].second
+      : max(dp(row + 1, col), dp(row + 1, col + 1)) + matrix[row][col].first;
+
+    matrix[row][col].second = ret;
+
+    return ret;
 }
 
 int main() {
     cin >> row_count;
 
     for (int row = 0; row < row_count; row++) {
-        matrix.push_back(vector<int>());
+        matrix.push_back(vector< pair<int, int> >());
         for (int col = 0; col <= row; col++) {
             int v;
             cin >> v;
-            matrix[row].push_back(v);
+            matrix[row].push_back(make_pair(v, -1));
         }
     }
 
